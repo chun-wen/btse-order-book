74bf96d0a8f54d598ad795d332ae0c97469bf57f
chunwen <hsbsbbdn@gmail.com>
2025-03-09 18:07:19 +0800

    chore: install prettier format

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+const config = {
+  trailingComma: "all",
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 80,
+  bracketSpacing: true,
+  arrowParens: "avoid",
+  endOfLine: "lf",
+};
+
+export default config;
